1adb65c39341968f001a94cd4b0b973d617f2e9f
Tsotne-mos <mosashvilitsotne@gnail.com>
2025-01-28 08:30:12 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let a =1
+while (a<=100) {
+    console.log(a);
+    a++;
+}
+
+
+
+
+for (let i = 0; i < 50; i++) {
+
+    if (i % 2) {
+        console.log( i );
+    }
+
+}
